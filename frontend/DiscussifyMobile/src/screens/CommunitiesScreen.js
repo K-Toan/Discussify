9e13e8671cb6112878a68aed378f3bf34a74a4d3
letoangt <81970719+K-Toan@users.noreply.github.com>
2024-10-02 14:08:24 +0700

    Added Communities card to CommunitiesScreen

--- frontend/DiscussifyMobile/src/screens/CommunitiesScreen.js
+++ frontend/DiscussifyMobile/src/screens/CommunitiesScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, FlatList } from 'react-native';
+import { Avatar, Card, Button } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
-import { theme } from '../components/theme'; // Import the current theme
+import { theme } from '../components/theme';
 
 const tags = [
     { id: 1, label: 'Game Development' },
@@ -14,22 +15,63 @@ const tags = [
     { id: 8, label: 'CSS' },
 ];
 
+const communities = [
+    { id: 1, name: 'Community 1', avatar: 'https://picsum.photos/120', totalMember: 72200, description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.' },
+    { id: 2, name: 'Community 2', avatar: 'https://picsum.photos/650', totalMember: 1400, description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.' }
+]
+
 export default function CommunitiesScreen() {
     const navigation = useNavigation();
 
     return (
         <View style={[styles.container, { backgroundColor: theme.background }]}>
-            <Text style={[styles.title, { color: theme.text }]}>Explore Communities by Topic:</Text>
-            <View style={styles.tagContainer}>
-                {tags.map((tag) => (
-                    <TouchableOpacity
-                        key={tag.id}
-                        style={[styles.tag, { backgroundColor: theme.iconActiveColor }]}
-                        onPress={() => navigation.navigate('CommunityDetails', { tagName: tag.label })}
-                    >
-                        <Text style={[styles.tagText, { color: theme.text }]}>{tag.label}</Text>
-                    </TouchableOpacity>
-                ))}
+            <View style={{ marginBottom: 10 }}>
+                <Text style={[styles.title, { color: theme.text }]}>Explore Communities by Topic:</Text>
+                <View style={styles.tagContainer}>
+                    {tags.map((tag) => (
+                        <TouchableOpacity
+                            key={tag.id}
+                            style={[styles.tag, { backgroundColor: theme.iconActiveColor }]}
+                            onPress={() => navigation.navigate('CommunityDetails', { tagName: tag.label })}
+                        >
+                            <Text style={[styles.tagText, { color: theme.text }]}>{tag.label}</Text>
+                        </TouchableOpacity>
+                    ))}
+                </View>
+            </View>
+            <View>
+                <Text style={[styles.title, { color: theme.text }]}>Recommended:</Text>
+                <FlatList
+                    data={communities}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({ item }) => (
+                        <View style={{ margin: 10 }}>
+                            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginBottom: 8 }}>
+                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                    <Image
+                                        source={{ uri: item.avatar }}
+                                        style={{ width: 36, height: 36, borderRadius: 20, marginRight: 10 }}
+                                    />
+                                    <View style={{ flexDirection: 'column', alignItems: 'flex-start' }}>
+                                        <Text style={{ fontWeight: 'bold', color: theme.text }}>
+                                            {item.name}
+                                        </Text>
+                                        <Text style={{ marginLeft: 0, fontSize: 12, color: 'gray' }}>{item.totalMember} members</Text>
+                                    </View>
+                                </View>
+
+                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                    <View style={{ flexDirection: 'column', alignItems: 'flex-start' }}>
+                                        <Button>Join</Button>
+                                    </View>
+                                </View>
+                            </View>
+                            <Text style={{ padding: 5, color: theme.subtext }} numberOfLines={2}>
+                                {item.description}
+                            </Text>
+                        </View>
+                    )}
+                />
             </View>
         </View>
     );
