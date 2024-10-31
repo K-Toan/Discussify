2d99c2e0562d28c014750dcaa519d56ed9e0fa6e
letoangt <81970719+K-Toan@users.noreply.github.com>
2024-10-31 12:59:01 +0700

    Added transaction to CreatePostCommandHandler

--- src/microservices/PostMicroservice/Application/Handlers/CreatePostCommandHandler.cs
+++ src/microservices/PostMicroservice/Application/Handlers/CreatePostCommandHandler.cs
@@ -5,22 +5,36 @@ using Contracts.MassTransit;
 using PostMicroservice.Models;
 using PostMicroservice.Application.Commands;
 using PostMicroservice.Infrastructure.Repositories;
+using PostMicroservice.Infrastructure;
 
 namespace PostMicroservice.Application.Hanlders;
 
-public class CreatePostCommandHandler(IMapper mapper, IPostRepository postRepository, IPublishEndpoint publishEndpoint) : IRequestHandler<CreatePostCommand, Post>
+public class CreatePostCommandHandler(IMapper mapper, PostDbContext context, IPostRepository postRepository, IPublishEndpoint publishEndpoint) : IRequestHandler<CreatePostCommand, Post>
 {
     public async Task<Post> Handle(CreatePostCommand request, CancellationToken cancellationToken)
     {
-        var post = await postRepository.AddAsync(mapper.Map<Post>(request));
+        using (var transaction = context.Database.BeginTransaction())
+        {
+            try
+            {
+                // add to db
+                var post = await postRepository.AddAsync(mapper.Map<Post>(request));
+                await postRepository.SaveChangesAsync();
 
-        await postRepository.SaveChangesAsync();
+                // publish to outbox
+                await publishEndpoint.Publish(new PostCreated(post.PostId, post.AuthorId, post.AuthorName, post.CommunityId, post.CommunityName, post.Title, post.CreatedAt));
+                await postRepository.SaveChangesAsync();
 
-        // publish
-        await publishEndpoint.Publish(new PostCreated(post.PostId, post.AuthorId, post.AuthorName, post.CommunityId, post.CommunityName, post.Title, post.CreatedAt));
-        
-        await postRepository.SaveChangesAsync();
+                transaction.Commit();
 
-        return post;
+                return post;
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+
+                throw new Exception("Create post transaction not completed!");
+            }
+        }
     }
 }
