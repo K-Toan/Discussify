0b326c96b49df00c7e74c4f5c6bc65846199e87c
K-Toan <988letoangt@gmail.com>
2024-11-01 12:33:48 +0700

    Modified gateway routes

--- src/gateways/OcelotApiGateway/Program.cs
+++ src/gateways/OcelotApiGateway/Program.cs
@@ -7,11 +7,6 @@ var config = builder.Configuration;
 // add ocelot services and routes
 builder.Services.AddOcelot();
 config.AddJsonFile("ocelot.json");
-config.AddJsonFile("routes/authentication-routes.json", optional: true, reloadOnChange: true);
-config.AddJsonFile("routes/post-routes.json", optional: true, reloadOnChange: true);
-config.AddJsonFile("routes/comment-routes.json", optional: true, reloadOnChange: true);
-config.AddJsonFile("routes/interaction-routes.json", optional: true, reloadOnChange: true);
-config.AddJsonFile("routes/subscription-routes.json", optional: true, reloadOnChange: true);
 
 var app = builder.Build();
 
